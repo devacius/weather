a68ac8d9c6a9d3a82532ed9753b26f07d71232d0
Deepansh Gupta <81462832+devacius@users.noreply.github.com>
2024-04-27 06:49:45 +0530

    on saturday

--- src/app/pages/tables/page.tsx
+++ src/app/pages/tables/page.tsx
@@ -3,9 +3,10 @@ import { DataTable } from "./data-table"
 
 async function getData(): Promise<Cities[]> {
   // Fetch data from your API here.
-  const res = await fetch(`https://${process.env.VERCEL_URL}/api/list_of_cities1`);
+    const res = await fetch('https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=100');
+  
     const data = await res.json()
-    return data.data.results;
+    return data.results;
 }
 
 export default async function DemoPage() {
@@ -14,6 +15,7 @@ export default async function DemoPage() {
  if(data){
   return (
     <div className="container mx-auto py-10 justify-center">
+      shit
       <DataTable columns={columns} data={data} />
     </div>
   )
