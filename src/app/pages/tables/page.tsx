3452f91cedbd3a55bff49139b32069748462a222
Deepansh Gupta <81462832+devacius@users.noreply.github.com>
2024-04-27 08:46:24 +0530

    next public

--- src/app/pages/tables/page.tsx
+++ src/app/pages/tables/page.tsx
@@ -11,7 +11,7 @@ async function getData(): Promise<Cities[]> {
 }
 
 export default  async function DemoPage() {
-  const data = getData()
+  const data = await getData()
  //console.log({data})
  if(data){
   return (
